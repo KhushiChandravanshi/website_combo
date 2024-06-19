dae4e2c85d703314223ba3b1853716d7ba21e928
khushi chandravanshi <khushichandravanshi377@gmail.com>
2024-06-20 00:31:18 +0530

    Add files via upload

---
+++ todayDeal.js
@@ -0,0 +1,117 @@
+export const todayDeal = [
+    {
+        img : "assets/box3-4.jpg",
+        discount : 32,
+        DealOfDay : "Deal of the Day",
+        desc : " Grand Gaming Days offers on Bestselling Gaming Laptops & Desktops - Exchange & No Cost EMI Available"
+    },
+    {
+        img : "assets/box3-3.jpg",
+        discount : 31,
+        DealOfDay : "Deal of the Day",
+        desc : "High Performance Handpicked Desktop Computers from HP, Lenovo and more"
+    },
+    {
+        img : "assets/product_img.jpg",
+        discount : 66,
+        DealOfDay : "Deal of the Day",
+        desc : "Best Prices on boAt Headphones, Soundbars and Speakers"
+    },
+    {
+        img : "assets/ipad_img.jpg",
+        discount : 80,
+        DealOfDay : "Deal of the Day",
+        desc : "Jaw dropping deals on headsets"
+    },
+
+    {
+        img : "assets/product2-5.jpg",
+        discount : 50,
+        DealOfDay : "Deal of the Day",
+        desc : "adidas & campus Footwear"
+    },
+    {
+        img : "assets/product2-6.jpg",
+        discount : 64,
+        DealOfDay : "Deal of the Day",
+        desc : "Grand Gaming Days Offers on Accessories and Storage Devices"
+    },
+
+    {
+        img : "assets/product1-7.jpg",
+        discount : 54,
+        DealOfDay : "Deal of the Day",
+        desc : "cooking essential"
+    },
+    {
+        img : "assets/product2-12.jpg",
+        discount : 52,
+        DealOfDay : "Deal of the Day",
+        desc : "TOP Deals on PUMA & Skechers footwear"
+    },
+
+    {
+        img : "assets/product2-11.jpg",
+        discount : 12,
+        DealOfDay : "Deal of the Day",
+        desc : "Handpicked Intel Powered Laptops; High Performance"
+    },
+    {
+        img : "assets/product1-3.jpg",
+        discount : 65,
+        DealOfDay : "Deal of the Day",
+        desc : "Powerbank from Mi, Ambrane, URBN and more"
+    },
+
+    {
+        img : "assets/product1-2.jpg",
+        discount : 65,
+        DealOfDay : "Deal of the Day",
+        desc : "Top deals on Truke, Govo and more"
+    },
+    {
+        img : "assets/product1-4.jpg",
+        discount : 75,
+        DealOfDay : "Deal of the Day",
+        desc : "Amazing deals on pTron, Jabra, Portronics"
+    },
+    {
+        img : "assets/product1-3.jpg",
+        discount : 82,
+        DealOfDay : "Deal of the Day",
+        desc : "Levi's, Allen Solly, ANNI DESIGNER, Janasya & more"
+    },
+    {
+        img : "assets/product2-5.jpg",
+        discount : 77,
+        DealOfDay : "Deal of the Day",
+        desc : "Exciting  deals on Mivi, Wecool, Wings"
+    },
+    {
+        img : "assets/product1-6.jpg",
+        discount : 20,
+        DealOfDay : "Deal of the Day",
+        desc : "Never before deals on HP printers"
+    },
+    {
+        img : "assets/product1-5.jpg",
+        discount : 19,
+        DealOfDay : "Deal of the Day",
+        desc : "Vivo Y75 - 18W fast charge, FHD plus display"
+    },
+    {
+        img : "assets/product2-9.jpg",
+        discount : "25",
+        DealOfDay : "Deal of the Day",
+        desc : "Nokia 5710 - newly launched 4G feature phone"
+    },
+    {
+        img : "assets/product2-4.jpg",
+        discount : 32,
+        DealOfDay : "Deal of the Day",
+        desc : "Vacuum Cleaners from Top Brands"
+    },
+
+
+
+]
